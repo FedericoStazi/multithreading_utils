d6edf0d3b3b358e10006885ba42f62a74870b6f1
FedericoStazi <stazi.federico@gmail.com>
2020-07-02 23:07:50 +0100

    Completed Semaphore

--- Semaphore.h
+++ Semaphore.h
@@ -10,66 +10,45 @@
 #include <mutex>
 #include <iostream>
 #include <condition_variable>
+#include <queue>
 
+// TODO: docs
 template<std::ptrdiff_t max_count = PTRDIFF_MAX>
 class Semaphore {
 
 public:
 
-    ~Semaphore() {
-        destroying = true;
-        rel_cv.notify_all();
-        acq_cv.notify_all();
-    }
-
-    /// increment internal counter
-    void release(std::ptrdiff_t update = 1) {
-
-        std::unique_lock<std::mutex> rel_lk(release_mutex);
-        rel_cv.wait(rel_lk, [&] { return counter + update <= max() or destroying; });
+    void clear() {
 
         {
-            std::lock_guard<std::mutex> acq_lk(acquire_mutex);
-            counter += update;
-            assert(counter <= max() or destroying);
+            std::lock_guard<std::mutex> tlk(release_queue.get_mutex());
+            release_queue.clear();
+            release_queue.notify_first();
         }
 
-        acq_cv.notify_one();
+        {
+            std::lock_guard<std::mutex> tlk(acquire_queue.get_mutex());
+            acquire_queue.clear();
+            acquire_queue.notify_first();
+        }
 
     }
 
-    /// decrement internal counter
+    void release() {
+        update<void>(+1, release_queue, acquire_queue);
+    }
+
     void acquire() {
+        update<void>(-1, acquire_queue, release_queue);
+    }
 
-        std::unique_lock<std::mutex> acq_lk(acquire_mutex);
-        acq_cv.wait(acq_lk, [&] { return counter - 1 >= 0 or destroying; });
-
-        {
-            std::lock_guard<std::mutex> rel_lk(release_mutex);
-            counter--;
-            assert(counter >= 0 or destroying);
-        }
-
-        rel_cv.notify_one();
-
+    bool try_release() {
+        return update<bool>(+1, release_queue, acquire_queue);
     }
 
     bool try_acquire() {
-
-        std::unique_lock<std::mutex> acq_lk(acquire_mutex);
-        std::lock_guard<std::mutex> rel_lk(release_mutex);
-
-        if (counter - 1 >= 0 or destroying) {
-            counter--;
-            rel_cv.notify_all();
-            assert(counter >= 0 or destroying);
-            return true;
-        }
-
-        return false;
-
+        return update<bool>(-1, acquire_queue, release_queue);
     }
-
 
     static constexpr inline std::ptrdiff_t max() {
         return max_count;
@@ -77,15 +56,95 @@ public:
 
 private:
 
+    struct WaitingThread {
+        std::mutex m;
+        std::condition_variable cv;
+        bool turn = false;
+    };
+
+    class WaitingThreadsQueue {
+
+    public:
+
+        inline std::mutex& get_mutex() { return mutex; }
+
+        inline bool is_clearing() { return clearing; }
+
+        inline bool clear() { clearing = true; }
+
+        bool empty() { return queue.empty(); }
+
+        void push(WaitingThread* wt) { return queue.push(wt); }
+
+        void pop() {
+            queue.pop();
+            if (!queue.empty())
+                queue.front()->turn = true;
+        }
+
+        void notify_first() {
+            if (!queue.empty())
+                queue.front()->cv.notify_one();
+        }
+
+    private:
+
+        std::queue<WaitingThread*> queue;
+        std::mutex mutex;
+        bool clearing = false;
+
+    } release_queue, acquire_queue;
+
     std::atomic<int> counter = 0;
 
-    std::mutex release_mutex;
-    std::mutex acquire_mutex;
+    template<class ReturnType> //ReturnType can only be void (for waiting update) or bool (for non-waiting update)
+    ReturnType update(int value, WaitingThreadsQueue& this_queue, WaitingThreadsQueue& other_queue) {
 
-    std::condition_variable rel_cv;
-    std::condition_variable acq_cv;
+        static_assert(
+            std::is_same<ReturnType, void>::value or std::is_same<ReturnType, bool>::value,
+            "ReturnType can only be void (for waiting update) or bool (for non-waiting update)");
 
-    bool destroying = false;
+        WaitingThread wt;
+        bool updated = true;
+
+        {
+            std::lock_guard<std::mutex> tlk(this_queue.get_mutex());
+            if(this_queue.empty())
+                wt.turn = true;
+            else if constexpr (!std::is_void<ReturnType>::value)
+                return false;
+            this_queue.push(&wt);
+        }
+
+        {
+            std::unique_lock<std::mutex> lk(wt.m);
+            while (!((counter + value >= 0 and counter + value <= max() and wt.turn) or this_queue.is_clearing())) {
+                if constexpr (!std::is_void<ReturnType>::value) {
+                    updated = false;
+                    break;
+                }
+                wt.cv.wait(lk);
+            }
+            if (updated)
+                counter += value;
+            assert((counter >= 0 and counter <= max()) or this_queue.is_clearing());
+        }
+
+        {
+            std::lock_guard<std::mutex> tlk(this_queue.get_mutex());
+            this_queue.pop();
+            this_queue.notify_first();
+        }
+
+        {
+            std::lock_guard<std::mutex> tlk(other_queue.get_mutex());
+            other_queue.notify_first();
+        }
+
+        if constexpr (!std::is_void<ReturnType>::value)
+            return updated;
+
+    }
 
 };
 
--- test/semaphore.cpp
+++ test/semaphore.cpp
@@ -5,13 +5,22 @@
 #include <atomic>
 #include <thread>
 #include <functional>
-#include <random>
 #include "gtest/gtest.h"
 #include "../Semaphore.h"
 
 class SemaphoreTest : public ::testing::Test {
 
 protected:
+
+    ~SemaphoreTest() override {
+        semaphore.clear();
+        for (auto& t:threads)
+            t.join();
+    }
+
+    SemaphoreTest() {
+        srand(std::chrono::system_clock::now().time_since_epoch().count());
+    }
 
     /// This method can be called by create some threads calling a function repeatedly
     ///
@@ -20,27 +29,32 @@ protected:
     /// \param try_acquire Argument passed to the function
     /// \param operations Number of times the function is called
     /// \return True if the counter is correct (>= 0), False otherwise
-    bool test_using(
-        std::function<void(CountingSemaphore&, std::atomic<int>&, bool, bool)> test_function,
-        bool more_acquires,
-        bool try_acquire,
-        int operations) {
+    bool test_using(bool more_acquires, bool try_acquire, int operations) {
 
         threads.reserve(kThreads);
 
         for (int t=0; t<kThreads; t++) {
-            threads.emplace_back([&, more_acquires, try_acquire](){
-                for (int i=0; i<operations; i++)
-                    test_function(semaphore, counter, more_acquires, try_acquire);
+            threads.emplace_back([=](){
+                for (int i=0; i<operations; i++) {
+                    if (rand()%100 < (more_acquires ? 80 : 40)) {
+                        if (try_acquire and semaphore.try_acquire()) {
+                            counter--;
+                        }
+                        if (!try_acquire) {
+                            semaphore.acquire();
+                            counter--;
+                        }
+                    } else {
+                        semaphore.release();
+                        counter++;
+                    }
+                }
             });
         }
 
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
 
-        for (auto& t:threads)
-            t.detach();
-
-        std::cerr<<counter<<std::endl;
+        std::cerr<<"counter: "<<counter<<std::endl;
         return counter >= 0;
     }
 
@@ -51,58 +65,41 @@ protected:
 
 };
 
-std::random_device rd;
-std::mt19937 gen(rd());
-std::uniform_int_distribution<> dist(0, 99);
-
-/// Function used by the threads to test the semaphore.
-///
-/// \param s Semaphore being tested.
-/// \param counter Atomic variable used to keep track of the operations.
-/// \param more_acquires If true, function does more acquires then releases (80% - 20%).
-/// If false, it does approximately the same number (50% - 50%).
-/// \param try_acquire If true uses try_acquire (non-blocking). If false uses acquire (blocking).
-void random_release_or_acquire(CountingSemaphore& s, std::atomic<int>& counter, bool more_acquires, bool try_acquire) {
-    if (dist(gen)%100 < (more_acquires ? 80 : 40)) {
-        if (try_acquire and s.try_acquire()) {
-            counter--;
-        }
-        if (!try_acquire) {
-            s.acquire();
-            counter--;
-        }
-    } else {
-        s.release();
-        counter++;
-    }
-}
-
-
 // Tests with more_acquires: true and try_acquire: true (see random_release_or_acquire documentation)
 TEST_F(SemaphoreTest, semaphore_MoreAcquires_TryAcquire_FewOperations) {
-    ASSERT_TRUE(test_using(random_release_or_acquire, true, true, 100));
+    ASSERT_TRUE(test_using(true, true, 100));
 }
 
 TEST_F(SemaphoreTest, semaphore_MoreAcquires_TryAcquire_ManyOperations) {
-    ASSERT_TRUE(test_using(random_release_or_acquire, true, true, 100000));
+    ASSERT_TRUE(test_using(true, true, 10000));
 }
 
 
 // Tests with more_acquires: false and try_acquire: true (see random_release_or_acquire documentation)
-TEST_F(SemaphoreTest, semaphore_noMoreAcquires_TryAcquire_FewOperations) {
-    ASSERT_TRUE(test_using(random_release_or_acquire, false, true, 100));
+TEST_F(SemaphoreTest, semaphore_LessAcquires_TryAcquire_FewOperations) {
+    ASSERT_TRUE(test_using(false, true, 100));
 }
 
-TEST_F(SemaphoreTest, semaphore_noMoreAcquires_TryAcquire_ManyOperations) {
-    ASSERT_TRUE(test_using(random_release_or_acquire, false, true, 100000));
+TEST_F(SemaphoreTest, semaphore_LessAcquires_TryAcquire_ManyOperations) {
+    ASSERT_TRUE(test_using(false, true, 10000));
+}
+
+
+// Tests with more_acquires: true and try_acquire: false (see random_release_or_acquire documentation)
+TEST_F(SemaphoreTest, semaphore_MoreAcquires_noTryAcquire_FewOperations) {
+    ASSERT_TRUE(test_using(true, false, 100));
+}
+
+TEST_F(SemaphoreTest, semaphore_MoreAcquires_noTryAcquire_ManyOperations) {
+    ASSERT_TRUE(test_using(true, false, 10000));
 }
 
 
 // Tests with more_acquires: false and try_acquire: false (see random_release_or_acquire documentation)
-TEST_F(SemaphoreTest, semaphore_noMoreAcquires_noTryAcquire_FewOperations) {
-    ASSERT_TRUE(test_using(random_release_or_acquire, false, false, 100));
+TEST_F(SemaphoreTest, semaphore_LessAcquires_noTryAcquire_FewOperations) {
+    ASSERT_TRUE(test_using(false, false, 100));
 }
 
-TEST_F(SemaphoreTest, semaphore_noMoreAcquires_noTryAcquire_ManyOperations) {
-    ASSERT_TRUE(test_using(random_release_or_acquire, false, false, 100000));
+TEST_F(SemaphoreTest, semaphore_LessAcquires_noTryAcquire_ManyOperations) {
+    ASSERT_TRUE(test_using(false, false, 10000));
 }
